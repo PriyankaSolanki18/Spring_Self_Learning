1d40e52cd40c5ef9216650ae8e2a6e9361d7bcca
priyankasolanki <priyankasolanki.1820@gmail.com>
2025-10-24 18:45:08 +0530

    Removing complete XML for Spring Configuration

---
+++ springcore/src/main/java/com/springcore/javaconfig/JavaConfig.java
@@ -0,0 +1,19 @@
+package com.springcore.javaconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+// this class will handle the configuration in place of xml file
+
+@Configuration // equivalent to config.xml file
+//@ComponentScan(basePackages = "com.springcore.javaconfig") // to tell the base package
+public class JavaConfig {
+	
+	@Bean
+	public Student getStudent() {  // Bean name will be same as method name we don't have to write it explicitly different 
+		//creating new Student object 
+		Student student = new Student();
+		return student;
+	}
+}
--- springcore/src/main/java/com/springcore/spel/Demo.java
+++ springcore/src/main/java/com/springcore/spel/Demo.java
@@ -10,6 +10,38 @@ public class Demo {
 	
 	@Value("#{1+3+4+8}")
 	private int y;
+	
+	@Value("#{T(java.lang.Math).sqrt(25)}")
+	private double z;
+	
+	@Value("#{T(java.lang.Math).E}")
+	private double e;
+	
+	@Value("#{new java.lang.String('Priyanka Solanki')}")
+	private String name;
+	
+	// by default value is false
+	@Value("#{8>3}")
+	private boolean isActive;
+	
+	public boolean isActive() {
+		return isActive;
+	}
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getZ() {
+		return z;
+	}
+	public void setZ(double z) {
+		this.z = z;
+	}
 	public int getX() {
 		return x;
 	}
@@ -22,9 +54,15 @@ public class Demo {
 	public void setY(int y) {
 		this.y = y;
 	}
+	public double getE() {
+		return e;
+	}
+	public void setE(double e) {
+		this.e = e;
+	}
 	@Override
 	public String toString() {
-		return "Demo [x=" + x + ", y=" + y + "]";
+		return "Demo [x=" + x + ", y=" + y + ", z=" + z + ", e=" + e + ", name=" + name + ", isActive=" + isActive
+				+ "]";
 	}
-	
 }
