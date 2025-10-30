0c7948fdc49b88f3a37b8573f5e56dd91751bd1d
priyankasolanki <priyankasolanki.1820@gmail.com>
2025-10-31 00:03:00 +0530

    .git

--- springcore/src/main/java/com/springcore/javaconfig/JavaConfig.java
+++ springcore/src/main/java/com/springcore/javaconfig/JavaConfig.java
@@ -11,9 +11,15 @@ import org.springframework.context.annotation.Configuration;
 public class JavaConfig {
 	
 	@Bean
+	public Samosa getSamosa() {
+		return new Samosa();
+	}
+	
+	@Bean
 	public Student getStudent() {  // Bean name will be same as method name we don't have to write it explicitly different 
 		//creating new Student object 
-		Student student = new Student();
+		Student student = new Student(getSamosa());
 		return student;
 	}
+	
 }
